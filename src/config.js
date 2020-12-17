02bc7cd97f956e5c4ed54ebb580d3264c75da5ed
kwistprod <kwist879@gmail.com>
2020-12-17 12:46:44 +0300

    changes

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    // port:5432,
-    // host:"ec2-54-247-103-43.eu-west-1.compute.amazonaws.com",
-    // user: "oqwyaxatvawess",
-    // password: "69249aee76fb63bc5d5b7631ab8d68eed8926ae09256756d2cb388d3c5ef0589",
-    // DB: "derpf6i4qppiac",
-    // dialect: "postgres",
-    port:8080,
-    host:"localhost",
-    user: "postgres",
-    password: "root",
-    DB: "onlineschool",
-    dialect: "postgres"
+    port:5432,
+    host:"ec2-54-247-103-43.eu-west-1.compute.amazonaws.com",
+    user: "oqwyaxatvawess",
+    password: "69249aee76fb63bc5d5b7631ab8d68eed8926ae09256756d2cb388d3c5ef0589",
+    DB: "derpf6i4qppiac",
+    dialect: "postgres",
+    // port:8080,
+    // host:"localhost",
+    // user: "postgres",
+    // password: "root",
+    // DB: "onlineschool",
+    // dialect: "postgres"
 }
