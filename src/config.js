ce507e9322a0a12f711190942b549b22e7b6e219
kwistprod <kwist879@gmail.com>
2020-12-17 10:44:23 +0300

    make changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port:5432,
-    host:"ec2-54-247-103-43.eu-west-1.compute.amazonaws.com:5432",
+    host:"ec2-54-247-103-43.eu-west-1.compute.amazonaws.com",
     user: "oqwyaxatvawess",
     password: "69249aee76fb63bc5d5b7631ab8d68eed8926ae09256756d2cb388d3c5ef0589",
     DB: "derpf6i4qppiac",
